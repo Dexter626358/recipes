b7fad7c2b8323386c2a165be4773bb813cbd91ce
Dexter <gusevsergey85@gmail.comm>
2023-11-18 09:12:31 +0300

    Superuser

--- myrecipes/admin.py
+++ myrecipes/admin.py
@@ -1,3 +1,7 @@
+# recipes/admin.py
 from django.contrib import admin
+from .models import Category, Recipe, RecipeCategory
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Recipe)
+admin.site.register(RecipeCategory)
--- myrecipes/models.py
+++ myrecipes/models.py
@@ -1,3 +1,37 @@
+# recipes/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    # Добавьте другие поля по вашему выбору
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    preparation_steps = models.TextField()
+    cooking_time = models.PositiveIntegerField()  # Время приготовления в минутах
+    image = models.ImageField(upload_to='recipe_images/', blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, through='RecipeCategory')  # Связь с категориями
+
+    # Добавьте другие поля по вашему выбору
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeCategory(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    # Добавьте другие поля по вашему выбору
+
+    def __str__(self):
+        return f"{self.recipe.title} - {self.category.name}"
